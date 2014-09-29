c6f5220803ed8002a8babf61d4e11b203a70aa85
bvarla <bharat.varla1@gmail.com>
2014-09-29 11:56:16 -0400

    Initial Commit

---
+++ MyClass/MyClass.h
@@ -0,0 +1,19 @@
+//
+//  MyClass.h
+//  MyClass
+//
+//  Created by Bharat Varla on 9/29/14.
+//  Copyright (c) 2014 Bharat Varla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyClass.
+FOUNDATION_EXPORT double MyClassVersionNumber;
+
+//! Project version string for MyClass.
+FOUNDATION_EXPORT const unsigned char MyClassVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyClass/PublicHeader.h>
+
+
